9951e6ec409348af0275468f03475daabf089e95
Andres Delgado <dlegadoandres0502@gmail.com>
2024-02-23 12:27:25 -0600

    Estan librerias y el struct del paciente

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+//Para estilizar textos en CMD:
+#include <iomanip>
+#include <ios> 
+using namespace std;
+
+struct paciente{
+    string nombre;
+    string apellido;
+    string enfermedad;
+    int id;
+};
+
+int main(){
+    
+}
